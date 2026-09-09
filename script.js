b8e6803a68b30482bb43b3b758e79926bf104b7d
Kota Sunagawa <sunagawa00493@gmail.com>
2026-09-09 18:41:36 +0900

    chore: 00ボタンの実装

--- script.js
+++ script.js
@@ -24,12 +24,12 @@ function render() {
 // 数字ボタンが押された時の処理
 function inputDigit(digit) {
   if (state.overwrite) {
-    state.currentValue = digit;
+    state.currentValue = digit === "00" ? "0" : digit;
     state.overwrite = false;
   } else {
     // 桁数が増えすぎないように少し制限をかける
     if (state.currentValue.replace('-', '').length >= 12) return;
-    state.currentValue = state.currentValue === '0' ? digit : state.currentValue + digit;
+    state.currentValue = state.currentValue === '0' ? digit === "00" ? "0" : digit : state.currentValue + digit;
   }
 }
 
